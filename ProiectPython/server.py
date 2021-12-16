71da07fc8c228b9f7e802fdf3df4dd8a630288a1
PricopCosminFII <79044611+PricopCosminFII@users.noreply.github.com>
2021-12-16 05:19:13 +0200

    Fixed bugs

--- ProiectPython/server.py
+++ ProiectPython/server.py
@@ -46,91 +46,85 @@ def multi_threaded_client(connection):
     global lock_for_answer
     global lock_guesser
     global lock_sender
-    try:
-        if game_type == '1':
-            minimum = sys.maxsize
-            for i in range(3):
-                number_of_tries = 0
-                won_guesser = 0
-                numb = get_random_number()
+    if game_type == '1':
+        minimum = sys.maxsize
+        for i in range(3):
+            number_of_tries = 0
+            won_guesser = 0
+            numb = get_random_number()
+            connection.sendall(str.encode(' <<<Guess The Number !' + 'Round ' + str(i + 1) + ' >>>\n'))
+            while won_guesser == 0:
+                response = int(connection.recv(2048).decode('utf-8'))
+                result = validation_answer(response, numb)
+                connection.sendall(str.encode(result[0]))
+                won_guesser = result[1]
+                number_of_tries += 1
+
+            if number_of_tries <= minimum:
+                minimum = number_of_tries
+        connection.sendall(str.encode('The best score is ' + str(minimum) + ' !\n'))
+    elif game_type == '2':
+        game_role = connection.recv(2048).decode('utf-8')
+        minimum = sys.maxsize
+        if guesser == 1 and game_role == '1':
+            connection.sendall(str.encode('\nSorry but this role has been taken!\n'))
+            connection.close()
+            return
+        elif guesser == 0 and game_role == '1':
+            connection.sendall(str.encode('\nYou received GUESSER role!\n'))
+        if sender == 1 and game_role == '2':
+            connection.sendall(str.encode('\nSorry but this role has been taken!\n'))
+            connection.close()
+            return
+        elif sender == 0 and game_role == '2':
+            connection.sendall(str.encode('\nYou received SENDER role!\n'))
+
+        if guesser == 0 or sender == 0:
+            if game_role == '1':
+                lock_guesser.acquire()
+                guesser = 1
+                lock_guesser.release()
+            elif game_role == '2':
+                lock_sender.acquire()
+                sender = 1
+                lock_sender.release()
+        for i in range(3):
+            won_guesser = 0
+            won_sender = 0
+            resp_sender = -1
+            number_of_tries = 0
+            n = -1
+            if game_role == '1':
+                while n == -1:
+                    pass
                 connection.sendall(str.encode(' <<<Guess The Number !' + 'Round ' + str(i + 1) + ' >>>\n'))
                 while won_guesser == 0:
                     response = int(connection.recv(2048).decode('utf-8'))
-                    result = validation_answer(response, numb)
+                    lock_for_answer.acquire()
+                    resp_sender = response
+                    lock_for_answer.release()
+                    result = validation_answer(response, n)
                     connection.sendall(str.encode(result[0]))
                     won_guesser = result[1]
                     number_of_tries += 1
-
                 if number_of_tries <= minimum:
                     minimum = number_of_tries
-            connection.sendall(str.encode('The best score is ' + str(minimum) + ' !\n'))
-        elif game_type == '2':
-            game_role = connection.recv(2048).decode('utf-8')
-            minimum = sys.maxsize
-            if guesser == 1 and game_role == '1':
-                connection.sendall(str.encode('\nSorry but this role has been taken!\n'))
-                connection.close()
-                return
-            elif guesser == 0 and game_role == '1':
-                connection.sendall(str.encode('\nYou received GUESSER role!\n'))
-            if sender == 1 and game_role == '2':
-                connection.sendall(str.encode('\nSorry but this role has been taken!\n'))
-                connection.close()
-                return
-            elif sender == 0 and game_role == '2':
-                connection.sendall(str.encode('\nYou received SENDER role!\n'))
-
-            if guesser == 0 or sender == 0:
-                if game_role == '1':
-                    lock_guesser.acquire()
-                    guesser = 1
-                    lock_guesser.release()
-                elif game_role == '2':
-                    lock_guesser.acquire()
-                    sender = 1
-                    lock_guesser.release()
-            for i in range(3):
-                won_guesser = 0
-                won_sender = 0
-                resp_sender = -1
-                number_of_tries = 0
-                n = -1
-                if game_role == '1':
-                    while n == -1:
+            elif game_role == '2':
+                while guesser == 0:
+                    pass
+                connection.sendall(str.encode(' <<<Guess The Number !' + 'Round ' + str(i + 1) + ' >>>\n'))
+                lock_for_number.acquire()
+                n = int(connection.recv(2048).decode('utf-8'))
+                lock_for_number.release()
+                while won_sender == 0:
+                    while resp_sender == -1:
                         pass
-                    connection.sendall(str.encode(' <<<Guess The Number !' + 'Round ' + str(i + 1) + ' >>>\n'))
+                    result = validation_answer(resp_sender, n)
+                    connection.sendall(str.encode(result[0]))
+                    won_sender = result[1]
                     resp_sender = -1
-                    while won_guesser == 0:
 
-                        response = int(connection.recv(2048).decode('utf-8'))
-                        lock_for_answer.acquire()
-                        resp_sender = response
-                        lock_for_answer.release()
-                        result = validation_answer(response, n)
-                        connection.sendall(str.encode(result[0]))
-                        won_guesser = result[1]
-                        if won_guesser == 0:
-                            resp_sender = -1
-                        number_of_tries += 1
-                    if number_of_tries <= minimum:
-                        minimum = number_of_tries
-                elif game_role == '2':
-                    while guesser == 0:
-                        pass
-                    connection.sendall(str.encode(' <<<Guess The Number !' + 'Round ' + str(i + 1) + ' >>>\n'))
-                    lock_for_number.acquire()
-                    n = int(connection.recv(2048).decode('utf-8'))
-                    lock_for_number.release()
-                    while won_sender == 0:
-                        while resp_sender == -1:
-                            pass
-                        result = validation_answer(resp_sender, n)
-                        connection.sendall(str.encode(result[0]))
-                        won_sender = result[1]
-            connection.sendall(str.encode('The best score is ' + str(minimum) + ' !\n'))
-    except connection.error as e:
-        print(str(e))
-        connection.close()
+        connection.sendall(str.encode('The best score is ' + str(minimum) + ' !\n'))
 
     connection.close()
     guesser = 0
